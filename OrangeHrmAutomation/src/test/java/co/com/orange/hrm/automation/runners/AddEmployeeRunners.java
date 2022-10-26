127d6cf68d4d9deb931e3ebdcd3487951f8f8911
Jesus Aguilar <aguilar1100@gmail.com>
2022-10-25 19:33:34 -0500

    Update AddEmployeeRunners.java

--- OrangeHrmAutomation/src/test/java/co/com/orange/hrm/automation/runners/AddEmployeeRunners.java
+++ OrangeHrmAutomation/src/test/java/co/com/orange/hrm/automation/runners/AddEmployeeRunners.java
@@ -3,8 +3,6 @@ package co.com.orange.hrm.automation.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"src/test/resources/features/add_an_employee.feature"}
-        , glue = {"co.com.orange.hrm.automation.stepdefinitions"}
-        , snippets = CucumberOptions.SnippetType.CAMELCASE)
+@CucumberOptions(features = {"src/test/resources/features/add_an_employee.feature"}, glue = {"co.com.orange.hrm.automation.stepdefinitions"}, snippets = CucumberOptions.SnippetType.CAMELCASE)
 public class AddEmployeeRunners extends AbstractTestNGCucumberTests {
 }
